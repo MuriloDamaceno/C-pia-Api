1f8dc19487ab9c3a36108e7fe09f4778398539bc
MuriloDamaceno <damacenomurilo01@gmail.com>
2026-06-16 09:10:15 -0300

    passando arquivos

---
+++ src/models/usuarioModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const usuarioSchema = new mongoose.Schema({
+    nome: { type: String, required: [true, 'Nome é obrigatório'] },
+    email: { type: String, required: [true, 'Email é obrigatório'], unique: true },
+    senha: { type: String, required: [true, 'Senha é obrigatória'] }
+});
+
+usuarioSchema.pre('save', async function () {
+    if (!this.isModified('senha')) return;
+    this.senha = await bcrypt.hash(this.senha, 10);
+});
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
